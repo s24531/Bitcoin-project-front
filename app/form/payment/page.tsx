0463c2c002896bbf72b41786883495c457da83b2
s24531 <91957063+s24531@users.noreply.github.com>
2024-06-13 19:47:02 +0200

    fix display planes datails

--- app/form/payment/page.tsx
+++ app/form/payment/page.tsx
@@ -1,4 +1,4 @@
-// Importowanie niezbędnych zależności
+"use client"
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useShoppingCart } from "../../../context/ShoppingCartContext"; 
